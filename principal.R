d8dd6d90cbdf766a10ad12640fffbb09789871ba
Charles Santos <thyarles@gmail.com>
2021-05-04 23:54:02 -0300

    Inicio de código para responder à primeira questão

--- principal.R
+++ principal.R
@@ -22,7 +22,7 @@ print('Carregando e/ou instalando bibliotecas necessárias...')
 # Ativa os nossos módulos no diretório R
 # ------------------------------------------------------------------------------
 print('Carregando módulos na pasta R...')
-  sinasc <- modules::use("R")
+  sinasc <- modules::use('R')
 
 
 # Leitura das tabelas auxiliares
@@ -86,7 +86,74 @@ print('Limpando e otimizando memória...')
 
 # Formata campos de acordo com a Estrutura do SINASC que acompanhou o BD
 # ------------------------------------------------------------------------------
-AMOSTRA <- sinasc$dicionario$aplicaEstrutura(AMOSTRA)
+print('Formatando campos da amostra de acordo com dicionário de dados...')
+  AMOSTRA <- sinasc$dicionario$aplicaEstrutura(AMOSTRA)
+
+
+# Importações para usar o GGPLOT2
+# A biblioteca libridate foi usada porque weekdate() não permite ordenação
+# ------------------------------------------------------------------------------
+print('Importando bibliotecas para gerar gráficos e manipular datas...')
+  if (!require('ggplot2')) install.packages('ggplot2')      # Plotagem de gráficos
+  if (!require('lubridate')) install.packages('lubridate')  # Conversão de datas
+
+
+# Configurações do tema do GGPLOT2
+# ------------------------------------------------------------------------------
+print('Configurando o tema do GGPLOT para um padrão do trabalho...')
+  sinasc$grafico$configuraTema()
+
+
+################################################################################
+# Resposta aos itens
+################################################################################
+print('Gerando dados para a resposta do item 1...')
+  # 1. Pode-se dizer que o número de partos varia entre os dias da semana?
+  #    Por que?
+  # ----------------------------------------------------------------------------
+
+  # Leitura da biblioteca libridate, uma vez que o weekdate não permite ordenação
+  library(lubridate)
+
+  # Criação de lista com Partos por Dia da Semana (PDS)
+  PDS <- wday(AMOSTRA$DTNASC)
+
+  # Configuração dos fatores para melhor idenbtificação dos dias da semana
+  PDS <- factor(PDS, levels = 1:7, labels = c('Dom.', 'Seg.', 'Ter.',
+                                              'Quar.', 'Qui.', 'Sex.', 'Sáb.'))
+
+  # GEração do gráfico com o número de partos por dia da semana
+  ggplot(as.data.frame(PDS), aes(x = PDS, fill = PDS)) +
+    # Gráfico tipo barras
+    geom_bar() +
+    # Nomes dos eixos, título e subtítulo
+    labs(x = 'Dias da semana', y = 'Quantidade de partos',
+         title = 'Número de partos por dia da semana',
+         subtitle = 'Registrados no Brasil em 2016',
+         caption = 'Fonte: SINASC 2016',
+         tag = 'Fig. 1') +
+    # Retira legenda, o objetivo das cores é apenas deixar o gráfico mais atrativo
+    theme(legend.position = 'none')
+
+  # Grafa figura em disco para uso no Word
+  sinasc$grafico$gravaEmDisco()
+
+
+
+ggplot(as.data.frame(PDS), aes(fill = PDS)) +
+  geom_col(position = 'dodge') +
+  labs(x = 'Idade dos alunos', y = 'Quantidade de alunos', fill = 'Consome inalantes?') +
+  geom_text(aes(label = Freq), colour = 'white', size = 3, vjust = 1.5, position = position_dodge(.9))
+
+
+
+ggplot(data = amostra_sem_NA) +
+  labs(x ='Tipo de parto', y ='Quantidade de partos')+
+  geom_bar(mapping = aes(x = PARTO, fill=PARTO))
+
+
+
+ggplot(AMOSTRA$PARTO)
 
 
 #-----------------------------------------------------------
@@ -99,15 +166,15 @@ amostra_sem_NA <- AMOSTRA[,c('DTNASCMAE', 'ESCMAE', 'RACACORMAE', 'PARTO')]
 
 
   amostra_sem_NA$ESCMAE <- factor(amostra_sem_NA$ESCMAE,
-       label = c('Nenhuma',"1 a 3 anos",'4 a 7 anos',
+       label = c('Nenhuma','1 a 3 anos','4 a 7 anos',
                  '8 a 11 anos','12 e mais', 'Ignorado'),
        level= c(1,2,3,4,5,9))
   amostra_sem_NA$RACACORMAE <- factor(amostra_sem_NA$RACACORMAE,
-        label = c("Branca","Preta",'Amarela','Parda','Indígena'),
+        label = c('Branca','Preta','Amarela','Parda','Indígena'),
         level= 1:5)
 
   amostra_sem_NA$PARTO <- factor(amostra_sem_NA$PARTO,
-                          label = c("Vaginal","Cesáreo",'Ignorado'),
+                          label = c('Vaginal','Cesáreo','Ignorado'),
                           level= c(1,2,9))
 
 
@@ -118,24 +185,24 @@ library(moments)
 
 
 ggplot(data = amostra_sem_NA) +
-  labs(x ="Tipo de parto", y ="Quantidade de partos")+
+  labs(x ='Tipo de parto', y ='Quantidade de partos')+
   geom_bar(mapping = aes(x = PARTO, fill=PARTO))
 
 
 ggplot(data = amostra_sem_NA) +
-  labs(x ="Escolaridade da Mãe", y ="Quantidade de pessoas por classe") +
+  labs(x ='Escolaridade da Mãe', y ='Quantidade de pessoas por classe') +
   geom_bar(mapping = aes(x = ESCMAE, fill=ESCMAE)) +
   coord_flip()
 
 ggplot(data = amostra_sem_NA) +
-  labs(x ="Identidade Racial", y ="Quantidade de pessoas por Raça") +
+  labs(x ='Identidade Racial', y ='Quantidade de pessoas por Raça') +
   geom_bar(mapping = aes(x = RACACORMAE, fill=RACACORMAE))
 
 
 
 ggplot(data = amostra_sem_NA) +
-  labs(x ="Tipo de Parto", y ="Frequencia relativa") +
-  geom_bar(mapping = aes(x = ESCMAE, fill = PARTO), position = "fill")
+  labs(x ='Tipo de Parto', y ='Frequencia relativa') +
+  geom_bar(mapping = aes(x = ESCMAE, fill = PARTO), position = 'fill')
 
 
 
