409d929e8b778cf72e4c6e28ce2998d4534a71ab
Bruno <marquesribeiro.bruno@gmail.com>
2021-05-05 21:11:07 -0300

    Atualizar gráficos da questão 5

--- R/q05.R
+++ R/q05.R
@@ -22,6 +22,7 @@ resposta <- function(df) {
                           (!is.na(parto$ESCMAE)) &
                           (!is.na(parto$RACACORMAE)),]
 
+
   # Geração do gráfico com o número de partos por dias úteis/fds
   ggplot(parto) +
     # Gráfico tipo barras
