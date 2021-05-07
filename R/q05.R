5ba1b240e2953bf366d687f31aff93d29be9d688
Bruno <marquesribeiro.bruno@gmail.com>
2021-05-07 10:49:24 -0300

    Atualização da q05

--- R/q05.R
+++ R/q05.R
@@ -71,6 +71,40 @@ resposta <- function(df) {
   grafico$gravaEmDisco('q01-partosPorEscolaridade')
 
 
+
+
+  #-- Gera o gráfico de pontos do tipo de partos a partir das idades das mães-
+  # Geração do gráfico com o número de partos pela idade das mães
+  tab_partos_idadeMAE <- table(parto$IDADEMAE,parto$PARTO )
+  #addmargins(tab_partos_idade)
+  print(tab_partos_idadeMAE)
+  provisorio <- data.frame(prop.table(tab_partos_idadeMAE,1))
+  DF_parto_idade <- provisorio[provisorio$Var2 == 'Vaginal',]
+  #Mudamos os nomes das variáveis
+  names(DF_parto_idade)[names(DF_parto_idade) == 'Freq'] <- 'Vaginal'
+  DF_parto_idade$Cesario <- provisorio$Freq[(provisorio$Var2 != 'Vaginal') &
+                                              (provisorio$Var2 != 'Ignorado')]
+  names(DF_parto_idade)[names(DF_parto_idade) == 'Var1'] <- 'Idade'
+  DF_parto_idade$Var2<-NULL
+
+
+  ggplot(DF_parto_idade) +
+    # Gráfico de pontos - vermelhos são Cesários e azuis vaginais
+    geom_point(aes(x = Idade, y = Cesario),color = 'red') +
+    geom_point(aes(x = Idade, y = Vaginal), color = 'blue') +
+    # Escala de cor leve
+    scale_fill_brewer() +
+    # Nomes dos eixos, título e subtítulo
+    labs(x = 'Idade da Mãe', y = 'Proporção de partos',
+         title = 'Tipos de parto pela idade da mãe',
+         subtitle = 'Registrados no Brasil em 2016',
+         caption = 'Fonte: SINASC 2016')
+     #Retira título da legenda e posiciona no topo
+    theme(legend.title = element_blank(), legend.position = "top")
+  # Grava figura em disco para uso no Word (veja no diretório png)
+  grafico$gravaEmDisco('q01-partosPorEscolaridade')
+
+
   #----- Gera o gráfico do número de partos a partir das idades agrupadas----
   # Geração do gráfico com o número de partos pela idade das mães
   ggplot(parto) +
@@ -107,33 +141,34 @@ resposta <- function(df) {
   grafico$gravaEmDisco('q01-partosPorEscolaridade')
 
 
+
   #----- Geração das tabelas de contingência --------------------------------
 
 
     # Gerando tabela de contingência partos por idade
   tab_partos_idade <- table(parto$IDADE_AGRUP,parto$PARTO )
   #addmargins(tab_partos_idade)
-  tab_partos_idade
+  print(droplevels(tab_partos_idade))
 
   # Proporção das idades das mães (Variável resposta) por parto (v. explicativa)
-  prop.table(tab_partos_idade,2)#*100
+  print(prop.table(tab_partos_idade,2))#*100
 
   # Proporção dos partos (Variável resposta) por idade (v. explicativa)
-  prop.table(tab_partos_idade,1)#*100
+  print(prop.table(tab_partos_idade,1))#*100
 
 
   # Tabela de contigência partos por escolaridade
   tab_partos_escolaridade <- table(parto$ESCMAE,parto$PARTO )
   #addmargins(tab_partos_escolaridade)
-  (tab_partos_escolaridade)
-  prop.table(tab_partos_escolaridade,1)#*100
+  print(tab_partos_escolaridade)
+  print(prop.table(tab_partos_escolaridade,1))#*100
 
 
   # Tabela de contigência partos por cor
   tab_partos_cor <- table(parto$RACACORMAE,parto$PARTO )
   #addmargins(tab_partos_cor)
-  (tab_partos_cor)
-  prop.table(tab_partos_cor,1)#*100
+  print(tab_partos_cor)
+  print(prop.table(tab_partos_cor,1))#*100
 
 
   }
